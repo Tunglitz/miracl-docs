c161aeed9677a13b6047caa68f101f3bc415112b
j8 <j8@j6.io>
2015-10-28 14:20:29 +0200

    fixes of config files

--- doc_miracl/js/customscripts.js
+++ doc_miracl/js/customscripts.js
@@ -1,5 +1,6 @@
 
-$('#asideAffix').height($(".nav").height());
+$('#mysidebar').height($(".nav").height());
+
 
 $( document ).ready(function() {
 
@@ -8,12 +9,7 @@ $( document ).ready(function() {
     var h = $(window).height();
     console.log (h);
     if (h > 800) {
-
-        $('#asideAffix').affix({
-          offset: {
-            top: 300
-          }
-        })
+        $( "#mysidebar" ).attr("class", "nav affix");
     }
     // activate tooltips. although this is a bootstrap js function, it must be activated this way in your theme.
     $('[data-toggle="tooltip"]').tooltip({
